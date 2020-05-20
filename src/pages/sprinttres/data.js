ee823d84665fc3f0cb1995faef68b4da6c3fb221
karla evelyn espino maldonado <karlaevelyn098@gmail.com>
2020-05-20 16:50:09 -0500

    cmp: Add exercise implement useState hook

---
+++ src/pages/sprinttres/data.js
@@ -0,0 +1,53 @@
+import hansei_1 from '../../assets/img/hansei_1.jpg';
+import hansei_2 from '../../assets/img/hansei_2.jpg';
+import mano_derecha_1 from '../../assets/img/mano_derecha_1.jpg';
+import mano_derecha_2 from '../../assets/img/mano_derecha_2.jpg';
+import ninja_1 from '../../assets/img/ninja_1.jpg';
+import ninja_2 from '../../assets/img/ninja_2.jpg';
+import tiempos_1 from '../../assets/img/tiempos_1.jpg';
+import tiempos_2 from '../../assets/img/tiempos_2.jpg';
+
+const data = [
+    {
+      img: hansei_1,
+      title: '¿Soy tu favorito?',
+      author: 'VO',
+    },
+    {
+      img: hansei_2,
+      title: '¿Soy tu favorito?',
+      author: 'VO',
+    },
+    {
+      img: mano_derecha_1,
+      title: '¿Soy tu favorito?',
+      author: 'VO',
+    },
+    {
+      img: mano_derecha_2,
+      title: '¿Soy tu favorito?',
+      author: 'VO',
+    },
+    {
+      img: ninja_2,
+      title: '¿Soy tu favorito?',
+      author: 'VO',
+    },
+    {
+      img: ninja_1,
+      title: '¿Soy tu favorito?',
+      author: 'VO',
+    },
+    {
+      img: tiempos_1,
+      title: '¿Soy tu favorito?',
+      author: 'VO',
+    },
+    {
+      img: tiempos_2,
+      title: '¿Soy tu favorito?',
+      author: 'VO',
+    }
+  ];
+
+ export default data;
